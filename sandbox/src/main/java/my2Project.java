9a6041de24ec6388a62e2b3afddea1ab6e2459d1
tha-u <deconstruction142@gmail.com>
2022-06-27 13:04:59 +0300

    Создан новый проект
    "Песочница"

---
+++ sandbox/src/main/java/my2Project.java
@@ -0,0 +1,9 @@
+public class my2Project {
+	public static void main(String[] args) {
+		for (int i = 0; i<args.length; i++){
+			System.out.print(args[i] + " 32 " + i);
+		}
+		
+		//System.out.println("Hello world!");
+	}
+}
